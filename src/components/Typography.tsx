6195d5d04e9d0dbb8b6ea2d0c0d86cecc53e94db
fioreenza <fiorenzanalle05@gmail.com>
2024-10-29 10:47:43 +0700

    feat: add prettier

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -75,57 +75,57 @@ export default function Typography<T extends React.ElementType>({
           variant === "h4" && [
             clsxm(
               "text-[48px] leading-[64px]",
-              "max-md:text-[30px] max-md:leading-[40px]"
+              "max-md:text-[30px] max-md:leading-[40px]",
             ),
           ],
           variant === "h5" && [
             clsxm(
               "text-[32px] leading-[36px]",
-              "max-md:text-[24px] max-md:leading-[32px]"
+              "max-md:text-[24px] max-md:leading-[32px]",
             ),
           ],
           variant === "h6" && [
             clsxm(
               "text-[28px] leading-[32px]",
-              "max-md:text-[20px] max-md:leading-[28px]"
+              "max-md:text-[20px] max-md:leading-[28px]",
             ),
           ],
           variant === "t" && [
             clsxm(
               "text-[20px] leading-[24px]",
-              "max-md:text-[18px] max-md:leading-[24px]"
+              "max-md:text-[18px] max-md:leading-[24px]",
             ),
           ],
           variant === "bl" && [
             clsxm(
               "text-[18px] leading-[24px]",
-              "max-md:text-[16px] max-md:leading-[20px]"
+              "max-md:text-[16px] max-md:leading-[20px]",
             ),
           ],
           variant === "bm" && [
             clsxm(
               "text-[16px] leading-[24px]",
-              "max-md:text-[14px] max-md:leading-[18px]"
+              "max-md:text-[14px] max-md:leading-[18px]",
             ),
           ],
           variant === "bs" && [
             clsxm(
               "text-[14px] leading-[18px]",
-              "max-md:text-[12px] max-md:leading-[16px]"
+              "max-md:text-[12px] max-md:leading-[16px]",
             ),
           ],
           variant === "c" && [
             clsxm(
               "text-[12px] leading-[16px]",
-              "max-md:text-[10px] max-md:leading-[14px]"
+              "max-md:text-[10px] max-md:leading-[14px]",
             ),
           ],
         ],
-        className
+        className,
       )}
       {...props}
     >
       {children}
     </Component>
   );
-} 
+}
