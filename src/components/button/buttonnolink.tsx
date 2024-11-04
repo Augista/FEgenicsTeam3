d621f191826803f1528796428892a18531a627e5
Bryan Michael Kurniawan <5026221025@mhs.its.ac.id>
2024-11-04 23:33:52 +0700

    feat: button without link

---
+++ src/components/button/buttonnolink.tsx
@@ -0,0 +1,59 @@
+// components/Button.js
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import React from "react";
+import Typography from "../Typography";
+import clsx from "clsx";
+
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+  children: ReactNode;
+  size?: "small" | "medium" | "large";
+  variant?: "primary" | "outline";
+  className?: string;
+}
+
+const ButtonNoLink = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, size = "medium", variant = "primary", className, ...rest }, ref) => {
+    // Define classes based on the variant prop
+    const variantClasses = {
+      primary: "bg-button text-white hover:bg-primary-dark focus:bg-primary-dark",
+      outline: "border-2 border-[#407BFF] text-[#407BFF] hover:text-white hover:bg-primary focus:text-white focus:bg-primary",
+    }
+    // Define classes based on the size prop
+    const sizeClasses = {
+      small: "px-6 py-1", // Small button styles
+      medium: "px-8 py-2", // Medium button styles
+      large: "px-10 py-3", // Large button styles
+    };
+
+    // Define typography variants based on the size
+    const typographyVariant: {
+      [key in "small" | "medium" | "large"]: "bs" | "bm" | "bl";
+    } = {
+      small: "bs", // Small button text variant
+      medium: "bm", // Medium button text variant
+      large: "bl", // Large button text variant
+    };
+    return (
+        <button
+          ref={ref}
+          className={clsx(`w-fit rounded-md ${sizeClasses[size]} ${variantClasses[variant]} ${className}`
+          )}
+          {...rest}
+        >
+          <Typography
+            as="span"
+            variant={typographyVariant[size]}
+            weight="medium"
+            className={`${variant === "outline" ? "text-[#407BFF]" : "text-white"}`}
+          >
+            {children}
+          </Typography>
+        </button>
+    );
+  }
+);
+
+ButtonNoLink.displayName = 'ButtonNoLink';
+
+export default ButtonNoLink;
